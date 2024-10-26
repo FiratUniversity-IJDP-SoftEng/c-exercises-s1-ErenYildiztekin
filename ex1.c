da1333cf25ec24d0b2fb419b8eb78554d8bd5407
ErenYildiztekin <144628328+ErenYildiztekin@users.noreply.github.com>
2024-10-26 18:28:25 +0300

    Update ex1.c

--- ex1.c
+++ ex1.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main() {
+    int secretNumber = 3;
+    int guess;
+
+    printf("1 ile 5 arasında bir sayı tahmin edin: ");
+    scanf("%d", &guess);  // Kullanıcıdan tahmini al
+
+    if (guess == secretNumber) {
+        printf("Tebrikler! Doğru tahmin.\n");
+    } else {
+        printf("Maalesef, yanlış tahmin. Doğru cevap %d idi.\n", secretNumber);
+    }
+    return 0;
+}
