1dd9ef1ebe3c9e207140903681e3ce59497eb945
ErenYildiztekin <144628328+ErenYildiztekin@users.noreply.github.com>
2024-10-26 18:29:33 +0300

    Update ex2.c

--- ex2.c
+++ ex2.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main() {
+    int age;
+
+    printf("Yaşınızı girin: ");
+    scanf("%d", &age);  
+
+    for (int i = 0; i < age; i++) {
+        printf("Happy Birthday!\n");
+    }
+
+    return 0;
+}
